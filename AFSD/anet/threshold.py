131a997e570c01764dca0069185d015cc96dfe59
Wentao Bao <wtbao2018@gmail.com>
2021-11-04 14:47:22 -0400

    update

--- AFSD/anet/threshold.py
+++ AFSD/anet/threshold.py
@@ -13,6 +13,7 @@ from AFSD.common.config import config
 import multiprocessing as mp
 import threading
 
+
 global result_dict
 result_dict = mp.Manager().dict()
 
@@ -50,6 +51,29 @@ def get_basic_config(config):
     return cfg
 
 
+def get_path(input_path):
+    if os.path.lexists(input_path):
+        fullpath = os.path.realpath(input_path) if os.path.islink(input_path) else input_path
+        real_name = fullpath.split('/')[-1]
+        real_full_path = os.path.join(os.path.dirname(input_path), real_name)
+    else:
+        raise FileNotFoundError
+    return real_full_path
+
+
+def prepare_data(video_name, center_crop):
+    sample_fps = cfg.video_infos[video_name]['fps']
+    duration = cfg.video_infos[video_name]['duration']
+    offsetlist = [0]
+    # get video data
+    data = np.load(os.path.join(cfg.mp4_data_path, video_name + '.npy'))
+    frames = data
+    frames = np.transpose(frames, [3, 0, 1, 2])
+    data = center_crop(frames)
+    data = torch.from_numpy(data.copy())
+    return data, offsetlist, sample_fps, duration
+
+
 def prepare_clip(data, offset, clip_length, crop_size):
     clip = data[:, offset: offset + clip_length]
     clip = clip.float()
@@ -62,16 +86,18 @@ def prepare_clip(data, offset, clip_length, crop_size):
     return clip
 
 
-def decode_prediction(output_dict, clip_length, score_func=nn.Softmax(dim=-1)):
-    loc, conf, priors = output_dict['loc'], output_dict['conf'], output_dict['priors']
-    prop_loc, prop_conf = output_dict['prop_loc'], output_dict['prop_conf']
-    center = output_dict['center']
-    loc = loc[0]
-    conf = score_func(conf[0])
-    prop_loc = prop_loc[0]
-    prop_conf = score_func(prop_conf[0])
-    center = center[0].sigmoid()
+def decode_prediction(output_dict, clip_length, score_func=nn.Softmax(dim=-1), use_edl=False, os_head=False):
+    # batchsize should be 1!
+    loc, conf, priors = output_dict['loc'][0], output_dict['conf'][0], output_dict['priors']
+    prop_loc, prop_conf = output_dict['prop_loc'][0], output_dict['prop_conf'][0]
+    center = output_dict['center'][0]
+    # conditional outputs
+    act = output_dict['act'][0].squeeze(-1) if os_head else None
+    prop_act = output_dict['prop_act'][0].squeeze(-1) if os_head else None
+    unct = output_dict['unct'][0] if use_edl else None
+    prop_unct = output_dict['prop_unct'][0] if use_edl else None
 
+    # decode the locations of segments
     pre_loc_w = loc[:, :1] + loc[:, 1:]
     loc = 0.5 * pre_loc_w * prop_loc + loc
     decoded_segments = torch.cat(
@@ -79,33 +105,64 @@ def decode_prediction(output_dict, clip_length, score_func=nn.Softmax(dim=-1)):
             priors[:, :1] * clip_length + loc[:, 1:]], dim=-1)
     decoded_segments.clamp_(min=0, max=clip_length)
 
+    # compute uncertainty and actionness
+    uncertainty = (unct + prop_unct) / 2.0 if use_edl else None
+    actionness = (act.sigmoid() + prop_act.sigmoid()) / 2.0 if os_head else None
+
+    # compute classification confidence
+    conf = score_func(conf)
+    prop_conf = score_func(prop_conf)
+    center = center.sigmoid()
+
     conf = (conf + prop_conf) / 2.0
-    conf = conf * center
+    conf = conf * center * actionness.unsqueeze(-1) if os_head else conf * center
     conf = conf.view(-1, cfg.num_classes).transpose(1, 0)
     conf_scores = conf.clone()
 
-    return decoded_segments, conf_scores
+    return decoded_segments, conf_scores, uncertainty, actionness
 
 
-def get_video_prediction(output, cuhk_class_1, cuhk_score_1, duration):
-    sum_count = 0
-    res = torch.zeros(cfg.num_classes, cfg.top_k, 3)
+def filtering(decoded_segments, conf_score_cls, uncertainty, actionness, offset, sample_fps, conf_thresh=1e-9, use_edl=False, os_head=False):
+    c_mask = conf_score_cls > conf_thresh
+    scores = conf_score_cls[c_mask]
+    if scores.size(0) == 0:
+        return None
+    l_mask = c_mask.unsqueeze(1).expand_as(decoded_segments)
+    segments = decoded_segments[l_mask].view(-1, 2)
+    segments = (segments + offset) / sample_fps
+    segments = torch.cat([segments, scores.unsqueeze(1)], -1)  # (N, 3)
+    if use_edl:
+        # masking uncertainties
+        uncertain_scores = uncertainty[c_mask]
+        segments = torch.cat([segments, uncertain_scores.unsqueeze(1)], -1)  # (N, 4)
+    if os_head:
+        # masking actionness
+        act_scores = actionness[c_mask]
+        segments = torch.cat([segments, act_scores.unsqueeze(1)], -1)  # (N, 4) or (N, 5)
+    return segments
+
+
+def get_video_prediction(output, pred_class, pred_conf, duration, use_edl=False, os_head=False, cls_rng=None):
+    res_dim = 3
+    res_dim = res_dim + 1 if use_edl else res_dim  # 3 or 4
+    res_dim = res_dim + 1 if os_head else res_dim  # 3 or 4 or 5
+    res = torch.zeros(cfg.num_classes, cfg.top_k, res_dim)
     # NMS for each class
-    for cl in range(1, cfg.num_classes):
+    for cl in cls_rng:
         if len(output[cl]) == 0:
             continue
         tmp = torch.cat(output[cl], 0)
-        tmp, count = softnms_v2(tmp, sigma=cfg.nms_sigma, top_k=cfg.top_k, score_threshold=1e-9)
+        tmp, count = softnms_v2(tmp, sigma=cfg.nms_sigma, top_k=cfg.top_k, score_threshold=1e-9, use_edl=use_edl, os_head=os_head)
         res[cl, :count] = tmp
-        sum_count += count
 
-    flt = res.contiguous().view(-1, 3)
-    flt = flt.view(cfg.num_classes, -1, 3)
+    flt = res.contiguous().view(-1, res_dim)
+    flt = flt.view(cfg.num_classes, -1, res_dim)
     proposal_list = []
-    for cl in range(1, cfg.num_classes):
-        class_name = cuhk_class_1
+    for cl in cls_rng:
+        cl_idx = cl + 1 if os_head else cl
+        class_name = pred_class  # assume the current video contains only one class
         tmp = flt[cl].contiguous()
-        tmp = tmp[(tmp[:, 2] > 0).unsqueeze(-1).expand_as(tmp)].view(-1, 3)
+        tmp = tmp[(tmp[:, 2] > 0).unsqueeze(-1).expand_as(tmp)].view(-1, res_dim)
         if tmp.size(0) == 0:
             continue
         tmp = tmp.detach().cpu().numpy()
@@ -115,10 +172,11 @@ def get_video_prediction(output, cuhk_class_1, cuhk_score_1, duration):
             end_time = min(duration, float(tmp[i, 1]))
             if end_time <= start_time:
                 continue
-
             tmp_proposal['label'] = class_name
-            tmp_proposal['score'] = float(tmp[i, 2]) * cuhk_score_1
+            tmp_proposal['score'] = float(tmp[i, 2]) * pred_conf
             tmp_proposal['segment'] = [start_time, end_time]
+            tmp_proposal['uncertainty'] = float(tmp[i, 3]) if use_edl else 0.0
+            tmp_proposal['actionness'] = float(tmp[i, 4]) if os_head else 0.0
             proposal_list.append(tmp_proposal)
     return proposal_list
 
@@ -126,10 +184,11 @@ def get_video_prediction(output, cuhk_class_1, cuhk_score_1, duration):
 def sub_processor(lock, pid, video_list, train_cls_data):
 
     out_layer = DirichletLayer(evidence=cfg.evidence, dim=-1) if cfg.use_edl else nn.Softmax(dim=-1)
-    centor_crop = videotransforms.CenterCrop(cfg.crop_size)
+    center_crop = videotransforms.CenterCrop(cfg.crop_size)
 
     cls_scores = train_cls_data["results"]  # (N, 200)
     cls_actions = train_cls_data["class"]  # idx_to_class (200)
+    class_range = range(1, cfg.num_classes) if not cfg.os_head else range(0, cfg.num_classes)
 
     text = 'processor %d' % pid
     with lock:
@@ -141,32 +200,20 @@ def sub_processor(lock, pid, video_list, train_cls_data):
         )
     torch.cuda.set_device(pid)
     net = BDNet(in_channels=cfg.input_channels,
-                training=False)
-    net.load_state_dict(torch.load(cfg.checkpoint_path))
+                training=False, use_edl=cfg.use_edl)
+    net.load_state_dict(torch.load(get_path(cfg.checkpoint_path)))
     net.eval().cuda()
 
     for video_name in video_list:
         # get the pre-loaded multi-class classification results
-        cuhk_score = cls_scores[video_name[2:]]
-        cuhk_class_1 = cls_actions[np.argmax(cuhk_score)]
-        cuhk_score_1 = max(cuhk_score)
+        scores = cls_scores[video_name[2:]]
+        pred_class = cls_actions[np.argmax(scores)]
+        pred_conf = max(scores)
 
         # get video information
-        sample_count = cfg.video_infos[video_name]['frame_num']
-        sample_fps = cfg.video_infos[video_name]['fps']
-        duration = cfg.video_infos[video_name]['duration']
-        offsetlist = [0]
-
-        # get video data
-        data = np.load(os.path.join(cfg.mp4_data_path, video_name + '.npy'))
-        frames = data
-        frames = np.transpose(frames, [3, 0, 1, 2])
-        data = centor_crop(frames)
-        data = torch.from_numpy(data.copy())
-
-        output = []
-        for cl in range(cfg.num_classes):
-            output.append([])
+        data, offsetlist, sample_fps, duration = prepare_data(video_name, center_crop)
+        
+        output = [[] for cl in range(cfg.num_classes)]
         for offset in offsetlist:
             # prepare clip of a video
             clip = prepare_clip(data, offset, cfg.clip_length, cfg.crop_size)
@@ -176,23 +223,18 @@ def sub_processor(lock, pid, video_list, train_cls_data):
                 output_dict = net(clip)
 
             # decode results
-            decoded_segments, conf_scores = decode_prediction(output_dict, cfg.clip_length, out_layer)
+            decoded_segments, conf_scores, uncertainty, actionness = decode_prediction(output_dict, cfg.clip_length, out_layer, use_edl=cfg.use_edl, os_head=cfg.os_head)
 
             # filtering
-            for cl in range(1, cfg.num_classes):
-                c_mask = conf_scores[cl] > 1e-9
-                scores = conf_scores[cl][c_mask]
-                if scores.size(0) == 0:
+            for cl in class_range:
+                segments = filtering(decoded_segments, conf_scores[cl], uncertainty, actionness, offset, sample_fps, use_edl=cfg.use_edl, os_head=cfg.os_head)
+                if segments is None:
                     continue
-                l_mask = c_mask.unsqueeze(1).expand_as(decoded_segments)
-                segments = decoded_segments[l_mask].view(-1, 2)
-                segments = (segments + offset) / sample_fps
-                segments = torch.cat([segments, scores.unsqueeze(1)], -1)
                 output[cl].append(segments)
         # finish offset loop
 
         # get final detection results for each video
-        result_dict[video_name[2:]] = get_video_prediction(output, cuhk_class_1, cuhk_score_1, duration)
+        result_dict[video_name[2:]] = get_video_prediction(output, pred_class, pred_conf, duration, use_edl=cfg.use_edl, os_head=cfg.os_head, cls_rng=class_range)
         with lock:
             progress.update(1)
     # finish video loop
@@ -245,10 +287,9 @@ def thresholding(cfg, output_file):
     threshold = compute_threshold(result_dict)
 
     output_dict = {"version": "ActivityNet-v1.3", "results": dict(result_dict), "external_data": {'threshold': threshold}}
-
     with open(output_file, "w") as out:
         json.dump(output_dict, out)
-
+    return threshold
 
 
 if __name__ == '__main__':
---
+++ experiments/anet_softmax/train.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+pwd_dir=$pwd
+cd ../../
+
+source activate afsd
+python setup.py develop
+
+GPU_ID=$1
+SPLIT=$2
+
+# run RGB model
+echo "Train the RGB model on ActivityNet1.3 Closed Set:"
+CUDA_VISIBLE_DEVICES=${GPU_ID} python AFSD/anet/train.py \
+    configs/anet_softmax.yaml \
+    --lw=1 \
+    --cw=10 \
+    --ctw=1 \
+    --ssl=0.001 \
+    --piou=0.6 \
+    --open_set \
+    --split=${SPLIT}
+
+
+cd $pwd_dir
+echo "Experiments finished!"
